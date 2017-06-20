f3b1b8148e7971857c947dd807657c2945391406
Factorio Blueprints <factorio.prints@gmail.com>
2017-06-20 07:10:41 -0400

    Remove source maps before deploying.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-git push -f origin HEAD:master && yarn install && yarn run build && firebase deploy && git tag prod HEAD -f && git push origin prod -f
+git push -f origin HEAD:master \
+	&& yarn install \
+	&& yarn run build \
+	&& rm build/static/css/main.*.css.map build/static/js/main.*.js.map \
+	&& firebase deploy \
+	&& git tag prod HEAD -f \
+	&& git push origin prod -f
