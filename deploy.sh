11e0222bcccc88e605c0c912dc905cf9ecf6176a
Factorio Blueprints <factorio.prints@gmail.com>
2017-06-19 21:21:17 -0400

    Change the deploy.sh script to push to the origin remote.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git push -f github HEAD:master && yarn install && yarn run build && firebase deploy && git tag prod HEAD -f && git push github prod -f
+git push -f origin HEAD:master && yarn install && yarn run build && firebase deploy && git tag prod HEAD -f && git push origin prod -f
