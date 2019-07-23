1c2a819e943f2360c0ec1c9811e6d8a14fee51e5
Klipsik <35381764+Klipsik@users.noreply.github.com>
2019-07-23 08:26:35 +0300

    Update buildImageUrl.js

--- src/helpers/buildImageUrl.js
+++ src/helpers/buildImageUrl.js
@@ -1,7 +1,7 @@
 const buildImageUrl = (imgurId, imgurType, suffix) =>
 {
 	const typeParts = imgurType.split('/');
-	return `https://i.imgur.com/${imgurId}${suffix}.${typeParts[1]}`;
+	return `http://i.imgur.com/${imgurId}${suffix}.${typeParts[1]}`;
 };
 
 export default buildImageUrl;
