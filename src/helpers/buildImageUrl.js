a489f8bbbd036495c57f7e7aa3c9caa470a1d1cf
oj <kristian.lisov@gmail.com>
2019-09-18 02:04:16 +0300

    fix scheme

--- src/helpers/buildImageUrl.js
+++ src/helpers/buildImageUrl.js
@@ -1,7 +1,7 @@
 const buildImageUrl = (imgurId, imgurType, suffix) =>
 {
 	const typeParts = imgurType.split('/');
-	return `http://i.imgur.com/${imgurId}${suffix}.${typeParts[1]}`;
+	return `//i.imgur.com/${imgurId}${suffix}.${typeParts[1]}`;
 };
 
 export default buildImageUrl;
