3a74e97a822c2573610915b5fcf27183c8b300a2
Factorio Blueprints <factorio.prints@gmail.com>
2020-08-16 14:23:02 -0400

    Add support for nested blueprint books.

--- src/parser/decodeFromBase64.js
+++ src/parser/decodeFromBase64.js
@@ -1,4 +1,5 @@
-import pako from 'pako';
+import {TextDecoder, TextEncoder} from 'fastestsmallesttextencoderdecoder';
+import pako                       from 'pako';
 
 const atob = require('atob');
 
@@ -34,10 +35,21 @@ export const decodeV15Base64 = (string) =>
 		arrayBuffer[i] = binary.charCodeAt(i);
 	}
 
-	const unzipped = pako.inflate(arrayBuffer);
-	const jsonCode = new TextDecoder('utf-8').decode(new Uint8Array(unzipped));
+	const unzipped    = pako.inflate(arrayBuffer);
+	const jsonCode = fromCharCode(unzipped);
+
 	return jsonCode;
 };
+
+function fromCharCode(bytes)
+{
+	let result = '';
+	for (var i = 0; i < bytes.byteLength; i++)
+	{
+		result += String.fromCharCode(bytes[i]);
+	}
+	return result;
+}
 
 export const encodeV15ToBase64 = (jsonCode) =>
 {
