90af1569e9c6a7483b39661b3d70cdc9bd4e204a
Factorio Blueprints <factorio.prints@gmail.com>
2021-02-20 23:10:01 -0500

    Move marked options from SingleBlueprint to BlueprintMarkdown.

--- src/components/single/BlueprintMarkdown.js
+++ src/components/single/BlueprintMarkdown.js
@@ -6,6 +6,28 @@ import PropTypes         from 'prop-types';
 import React             from 'react';
 import {useQuery}        from 'react-query';
 
+const renderer = new marked.Renderer();
+renderer.table = (header, body) => `<table class="table table-striped table-bordered">
+<thead>
+${header}</thead>
+<tbody>
+${body}</tbody>
+</table>
+`;
+renderer.image = (href, title, text) =>
+	`<img src="${href}" alt="${text}" class="img-responsive">`;
+
+marked.setOptions({
+	renderer,
+	gfm        : true,
+	tables     : true,
+	breaks     : false,
+	pedantic   : false,
+	sanitize   : false,
+	smartLists : true,
+	smartypants: false,
+});
+
 BlueprintMarkdown.propTypes = {
 	blueprintKey: PropTypes.string.isRequired,
 };
