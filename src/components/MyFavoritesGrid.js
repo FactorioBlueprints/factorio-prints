babc1c0f300a44aceaaa67ff974cf8b76721fe16
Factorio Blueprints <factorio.prints@gmail.com>
2021-03-02 16:04:27 -0500

    Write an efficient version of the My Favorites grid.

--- src/components/MyFavoritesGrid.js
+++ src/components/MyFavoritesGrid.js
@@ -1,118 +1,14 @@
-import {faCog} from '@fortawesome/free-solid-svg-icons';
-
-import {FontAwesomeIcon}      from '@fortawesome/react-fontawesome';
-import {forbidExtraProps}     from 'airbnb-prop-types';
-import PropTypes              from 'prop-types';
-import React, {PureComponent} from 'react';
-import Container              from 'react-bootstrap/Container';
-import Jumbotron              from 'react-bootstrap/Jumbotron';
-import Row                    from 'react-bootstrap/Row';
-import {connect}              from 'react-redux';
-import {bindActionCreators}   from 'redux';
-
-import {filterOnTags} from '../actions/actionCreators';
-
-import * as propTypes             from '../propTypes';
-import BlueprintSummaryProjection from '../propTypes/BlueprintSummaryProjection';
-import * as selectors             from '../selectors';
-
-import BlueprintThumbnail  from './BlueprintThumbnail';
-import PageHeader          from './PageHeader';
-import EfficientSearchForm from './search/EfficientSearchForm';
-import EfficientTagForm    from './search/EfficientTagForm';
+import React, {PureComponent}   from 'react';
+import EfficientMyFavoritesGrid from './grid/EfficientMyFavoritesGrid';
 
 class MyFavoritesGrid extends PureComponent
 {
-	static propTypes = forbidExtraProps({
-		filterOnTags             : PropTypes.func.isRequired,
-		user                     : propTypes.userSchema,
-		blueprintSummaries       : PropTypes.arrayOf(BlueprintSummaryProjection).isRequired,
-		blueprintSummariesLoading: PropTypes.bool,
-		location                 : propTypes.locationSchema,
-		history                  : propTypes.historySchema,
-		staticContext            : PropTypes.shape(forbidExtraProps({})),
-		match                    : PropTypes.shape(forbidExtraProps({
-			params : PropTypes.shape(forbidExtraProps({})).isRequired,
-			path   : PropTypes.string.isRequired,
-			url    : PropTypes.string.isRequired,
-			isExact: PropTypes.bool.isRequired,
-		})),
-	});
-
 	render()
 	{
-		if (!this.props.user)
-		{
-			return (
-				<Jumbotron>
-					<h1 className='display-4'>
-						My Favorites
-					</h1>
-					<p className='lead'>
-						Please log in with Google or GitHub in order to view your favorite blueprints.
-					</p>
-				</Jumbotron>
-			);
-		}
-
-		if (this.props.blueprintSummariesLoading)
-		{
-			return (
-				<Jumbotron>
-					<h1 className='display-4'>
-						<FontAwesomeIcon icon={faCog} spin />
-						{' Loading data'}
-					</h1>
-				</Jumbotron>
-			);
-		}
-
 		return (
-			<Container fluid>
-				<PageHeader title='My Favorites' />
-				<Row>
-					<EfficientSearchForm />
-					<EfficientTagForm />
-				</Row>
-				{
-					this.props.blueprintSummariesLoading
-						? this.renderLoadingSpinner()
-						: this.renderMainGrid()
-				}
-			</Container>
+			<EfficientMyFavoritesGrid />
 		);
 	}
-
-	renderLoadingSpinner = () =>
-		<Jumbotron>
-			<h1 className='display-4'>
-				<FontAwesomeIcon icon={faCog} spin />
-				{' Loading data'}
-			</h1>
-		</Jumbotron>;
-
-	renderMainGrid = () =>
-		<Row className='justify-content-center'>
-			{
-				this.props.blueprintSummaries.map(blueprintSummary =>
-					<BlueprintThumbnail key={blueprintSummary.key} blueprintSummary={blueprintSummary} />)
-			}
-		</Row>;
 }
 
-const mapStateToProps = storeState => (
-	{
-		user                     : selectors.getFilteredUser(storeState),
-		blueprintSummaries       : storeState.blueprintMyFavorites.data,
-		blueprintSummariesLoading: storeState.blueprintMyFavorites.loading,
-	});
-
-const mapDispatchToProps = (dispatch) =>
-{
-	const actionCreators = {
-		filterOnTags,
-	};
-	return bindActionCreators(actionCreators, dispatch);
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(MyFavoritesGrid);
+export default MyFavoritesGrid;
---
+++ src/components/grid/EfficientMyFavoritesGrid.js
@@ -0,0 +1,108 @@
+import {faCog}                       from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon}             from '@fortawesome/react-fontawesome';
+import axios                         from 'axios';
+import React, {useContext, useState} from 'react';
+import Container                     from 'react-bootstrap/Container';
+import Row                           from 'react-bootstrap/Row';
+import {useQuery}                    from 'react-query';
+
+import SearchContext from '../../context/searchContext';
+import UserContext   from '../../context/userContext';
+import getHeaders    from '../../helpers/getHeaders';
+
+import BlueprintThumbnail  from '../BlueprintThumbnail';
+import PageHeader          from '../PageHeader';
+import EfficientSearchForm from '../search/EfficientSearchForm';
+import EfficientTagForm    from '../search/EfficientTagForm';
+import PaginationControls  from './PaginationControls';
+
+EfficientMyFavoritesGrid.propTypes = {};
+
+function EfficientMyFavoritesGrid(props)
+{
+	const [page, setPage]             = useState(1);
+	const {titleFilter, selectedTags} = useContext(SearchContext);
+
+	const fetchBlueprintSummaries = async (page = 1, titleFilter, selectedTags, user) =>
+	{
+		const url    = `${process.env.REACT_APP_REST_URL}/api/my/favoriteBlueprints/page/${page}`;
+		const params = new URLSearchParams();
+		params.append('title', titleFilter);
+		selectedTags.forEach(tag => params.append('tag', tag.value));
+
+		const idToken = user === undefined ? undefined : await user.getIdToken();
+
+		const options = {
+			params,
+			headers: {
+				Authorization: `Bearer ${idToken}`,
+			}
+		};
+
+		const result = await axios.get(url, options);
+		return result.data;
+	};
+
+	const {user}       = useContext(UserContext);
+	const queryEnabled = user !== undefined;
+	const email        = user === undefined ? undefined : user.email;
+	const queryKey     = [email, 'my', 'favoriteBlueprints', page, titleFilter, selectedTags];
+
+	const placeholderData = {_data: [], _metadata: {pagination: {numberOfPages: 0, pageNumber: 0}}};
+	const options = {
+		keepPreviousData: true,
+		placeholderData,
+		enabled         : queryEnabled,
+	};
+	const result          = useQuery(queryKey, () => fetchBlueprintSummaries(page, titleFilter, selectedTags, user), options);
+
+	// TODO: Refactor out grid commonality
+
+	const {isSuccess, isLoading, isError, data, isPreviousData} = result;
+
+	if (isError)
+	{
+		console.log({result});
+		return (
+			<>
+				{'Error loading blueprint summaries.'}
+			</>
+		);
+	}
+
+	const {_data: blueprintSummaries = [], _metadata: {pagination: {numberOfPages = 0, pageNumber = 0}}} = data || placeholderData;
+
+	return (
+		<Container fluid>
+			<PageHeader title='My Favorites' />
+			<Row>
+				<EfficientSearchForm />
+				<EfficientTagForm />
+			</Row>
+			{isLoading && <Row>
+				<FontAwesomeIcon icon={faCog} size='lg' fixedWidth spin />
+				{' Loading blueprints'}
+			</Row>}
+			<Row className='justify-content-center'>
+				{
+					blueprintSummaries.map(blueprintSummary =>
+						(
+							<BlueprintThumbnail
+								key={blueprintSummary.key}
+								blueprintSummary={blueprintSummary}
+							/>
+						))
+				}
+			</Row>
+			<PaginationControls
+				page={page}
+				setPage={setPage}
+				pageNumber={pageNumber}
+				numberOfPages={numberOfPages}
+				isPreviousData={isPreviousData}
+			/>
+		</Container>
+	);
+}
+
+export default EfficientMyFavoritesGrid;
