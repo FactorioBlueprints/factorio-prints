4a2c5253d07a9e7bbc5bdda583aa8fef993fb950
Factorio Blueprints <factorio.prints@gmail.com>
2021-04-03 18:14:13 -0400

    fixup! Refactor more components to be functions.

--- src/components/EfficientAccount.js
+++ src/components/EfficientAccount.js
@@ -1,13 +1,47 @@
-import React, {useContext} from 'react';
-import UserContext         from '../context/userContext';
+import {faBan, faSave}               from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon}             from '@fortawesome/react-fontawesome';
+import React, {useContext, useState} from 'react';
+import Button                        from 'react-bootstrap/Button';
+import Col                           from 'react-bootstrap/Col';
+import Container                     from 'react-bootstrap/Container';
+import Form                          from 'react-bootstrap/Form';
+import FormControl                   from 'react-bootstrap/FormControl';
+import Jumbotron                     from 'react-bootstrap/Jumbotron';
+import Row                           from 'react-bootstrap/Row';
+import {useHistory}                  from 'react-router-dom';
+import UserContext                   from '../context/userContext';
+import PageHeader                    from './PageHeader';
 
 EfficientAccount.propTypes = {
-
 };
 
-function EfficientAccount(props)
+function EfficientAccount()
 {
 	const {user}       = useContext(UserContext);
+	const [displayName, setDisplayName] = useState(user.displayName);
+
+	const history = useHistory();
+
+	function handleChange(event)
+	{
+		event.preventDefault();
+		setDisplayName(event.target.value);
+	}
+
+	function handleSaveAccount(event)
+	{
+		event.preventDefault();
+
+		// TODO: POST new displayName at user.uid
+
+		history.push(`user/${user.uid}`);
+	}
+
+	function handleCancel(event)
+	{
+		event.preventDefault();
+		history.push(`user/${user.uid}`);
+	}
 
 	if (!user)
 	{
@@ -24,7 +58,50 @@ function EfficientAccount(props)
 	}
 
 	return (
-		<div></div>
+		<Container>
+			<PageHeader title='Edit account settings' />
+			<Row>
+				<Form className='w-100'>
+					<Form.Group as={Row}>
+						<Form.Label column sm='2'>
+							{'Display Name'}
+						</Form.Label>
+						<Col sm={10}>
+							<FormControl
+								autoFocus
+								type='text'
+								name='displayName'
+								placeholder='DisplayName'
+								value={displayName}
+								onChange={handleChange}
+							/>
+						</Col>
+					</Form.Group>
+
+					<Form.Group as={Row}>
+						<Col sm={{span: 10, offset: 2}}>
+							<Button
+								type='button'
+								variant='warning'
+								size='lg'
+								onClick={handleSaveAccount}
+							>
+								<FontAwesomeIcon icon={faSave} size='lg' />
+								{' Save'}
+							</Button>
+							<Button
+								type='button'
+								size='lg'
+								onClick={handleCancel}
+							>
+								<FontAwesomeIcon icon={faBan} size='lg' />
+								{' Cancel'}
+							</Button>
+						</Col>
+					</Form.Group>
+				</Form>
+			</Row>
+		</Container>
 	);
 }
 
