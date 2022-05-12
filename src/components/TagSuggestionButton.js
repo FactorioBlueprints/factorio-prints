5682bec907159be7320888c81fd339dd7849eb7d
Factorio Blueprints <factorio.prints@gmail.com>
2022-05-11 20:46:17 -0400

    Rewrite TagSuggestionButton component to be a pure function.

--- src/components/TagSuggestionButton.js
+++ src/components/TagSuggestionButton.js
@@ -1,34 +1,28 @@
-import {faTag}                from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon}      from '@fortawesome/react-fontawesome';
-import {forbidExtraProps}     from 'airbnb-prop-types';
-import PropTypes              from 'prop-types';
-import React, {PureComponent} from 'react';
-import Button                 from 'react-bootstrap/Button';
+import {faTag}            from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
+import {forbidExtraProps} from 'airbnb-prop-types';
+import PropTypes          from 'prop-types';
+import React              from 'react';
+import Button             from 'react-bootstrap/Button';
 
-export default class TagSuggestionButton extends PureComponent
+function TagSuggestionButton({addTag, tagSuggestion})
 {
-	static propTypes = forbidExtraProps({
-		tagSuggestion: PropTypes.string.isRequired,
-		addTag       : PropTypes.func.isRequired,
-	});
-
-	handleClick = () =>
-	{
-		this.props.addTag(this.props.tagSuggestion);
-	};
-
-	render()
-	{
-		return (
-			<Button
-				variant='warning'
-				type='button'
-				onClick={this.handleClick}
-			>
-				<FontAwesomeIcon icon={faTag} />
-				{' '}
-				{this.props.tagSuggestion}
-			</Button>
-		);
-	}
+	return (
+		<Button
+			variant='warning'
+			type='button'
+			onClick={() => addTag(tagSuggestion)}
+		>
+			<FontAwesomeIcon icon={faTag} />
+			{' '}
+			{tagSuggestion}
+		</Button>
+	);
 }
+
+TagSuggestionButton.propTypes = forbidExtraProps({
+	tagSuggestion: PropTypes.string.isRequired,
+	addTag       : PropTypes.func.isRequired,
+});
+
+export default TagSuggestionButton;
