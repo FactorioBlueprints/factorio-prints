7c2fda45a83d21aa9de6b66e8af6f2a728e531a2
Factorio Blueprints <factorio.prints@gmail.com>
2022-05-11 22:15:28 -0400

    Delete sagas and reducers.

--- src/index.js
+++ src/index.js
@@ -1,91 +1,20 @@
-import isEmpty   from 'lodash/isEmpty';
-import mapValues from 'lodash/mapValues';
-import pickBy    from 'lodash/pickBy';
-
-import throttle from 'lodash/throttle';
-import React    from 'react';
-import { createRoot } from 'react-dom/client';
-
-import {Provider} from 'react-redux';
-
-import {applyMiddleware, compose, createStore} from 'redux';
-import createSagaMiddleware                    from 'redux-saga';
-import Root                                    from './components/Root';
+import React        from 'react';
+import {createRoot} from 'react-dom/client';
+import Root         from './components/Root';
 
 import './css/style.css';
 
-import {saveState} from './localStorage';
-
-import rootReducer     from './reducers/rootReducer';
 import reportWebVitals from './reportWebVitals';
-import rootSaga        from './sagas/rootSaga';
 
-// Create the saga middleware
-const sagaMiddleware = createSagaMiddleware();
-
-// Const preloadedState = loadState();
-
-// Mount it on the Store
-const store = createStore(
-	rootReducer,
-	// PreloadedState,
-	compose(applyMiddleware(sagaMiddleware)),
-);
-
-store.subscribe(throttle(() =>
-{
-	const state = store.getState();
-
-	const {
-		blueprints,
-		blueprintSummaries: {
-			data: blueprintSummariesData,
-		},
-		tags              : {
-			data: tagsData,
-			tagHierarchy,
-		},
-		byTag,
-		filteredTags,
-		users,
-	} = state;
-
-	saveState({
-		blueprints        : mapValues(pickBy(blueprints, ({data}) => !isEmpty(data)), ({data}) => ({data})),
-		blueprintSummaries: {
-			data: blueprintSummariesData,
-		},
-		tags: {
-			data: tagsData,
-			tagHierarchy,
-		},
-		byTag: mapValues(byTag, ({data}) => ({data})),
-		filteredTags,
-		users: mapValues(pickBy(users, ({data}) => !isEmpty(data)), ({displayName: {data: displayNameData}, blueprints: {data: blueprintsData}}) =>
-			({
-				displayName: {
-					data: displayNameData,
-				},
-				blueprints: {
-					data: blueprintsData,
-				},
-			})),
-	});
-}, 1000));
-
-sagaMiddleware.run(rootSaga);
-
-const rootWrapper = (
-	<Provider store={store}>
-		<React.StrictMode>
-			<Root />
-		</React.StrictMode>
-	</Provider>
+const strictRoot = (
+	<React.StrictMode>
+		<Root />
+	</React.StrictMode>
 );
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(rootWrapper);
+root.render(strictRoot);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
