90436107aaae8f377b08d42071a29559ffa3b2ea
Factorio Blueprints <factorio.prints@gmail.com>
2022-12-18 15:30:41 -0500

    Remove unused prop types.

--- src/propTypes.js
+++ src/propTypes.js
@@ -11,15 +11,3 @@ export const byTagSchema = PropTypes.objectOf(PropTypes.shape(forbidExtraProps({
 	loading: PropTypes.bool.isRequired,
 	data   : PropTypes.objectOf(PropTypes.bool.isRequired).isRequired,
 })).isRequired).isRequired;
-
-export const locationSchema = PropTypes.shape(forbidExtraProps({
-	pathname: PropTypes.string.isRequired,
-	search  : PropTypes.string.isRequired,
-	hash    : PropTypes.string.isRequired,
-	state   : PropTypes.string,
-	key     : PropTypes.string,
-}));
-
-export const historySchema = PropTypes.shape({
-	push: PropTypes.func.isRequired,
-});
