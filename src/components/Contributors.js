f31a098414882d7efd8b580e29311f4fd7a9ff9d
Factorio Blueprints <factorio.prints@gmail.com>
2023-08-30 09:18:00 -0400

    Update Patreon contributors.

--- src/components/Contributors.js
+++ src/components/Contributors.js
@@ -5,7 +5,7 @@ import Alert                  from 'react-bootstrap/Alert';
 
 import React from 'react';
 
-const contributors = ['Tomáš Hubka', 'wisefish', 'Clive Blackledge', 'ensoniq2k']
+const contributors = ['Howard F.', 'Tomáš Hubka', 'wisefish', 'Clive Blackledge', 'ensoniq2k']
 
 function Contributors()
 {
