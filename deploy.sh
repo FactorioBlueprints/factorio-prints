1ec53c63fb4889a75b95af0baa7c74c15f242d0d
Factorio Blueprints <factorio.prints@gmail.com>
2024-11-11 19:31:43 -0500

    Improve deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+set -exuo pipefail
+
 git pushf open-source HEAD:factorio-prints.com \
 	&& git pushf open-source HEAD:factorio-prints.com \
-	&& yarn install \
+	&& yarn install --ignore-optional --ignore-engines --ignore--platform --ignore-scripts \
 	&& yarn run build \
 	&& firebase deploy \
 	&& git tag prod HEAD -f \
