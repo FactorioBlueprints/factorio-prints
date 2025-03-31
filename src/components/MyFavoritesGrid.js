e33ef65bba1ba089dc1e1c8ebfcfbd67dbbb8677
Factorio Blueprints <factorio.prints@gmail.com>
2025-03-31 13:59:52 -0400

    Convert MyFavoritesGrid to functional component

--- src/components/MyFavoritesGrid.js
+++ src/components/MyFavoritesGrid.js
@@ -1,13 +1,13 @@
 import {faCog} from '@fortawesome/free-solid-svg-icons';
 
-import {FontAwesomeIcon}      from '@fortawesome/react-fontawesome';
-import {forbidExtraProps}     from 'airbnb-prop-types';
-import PropTypes              from 'prop-types';
-import React, {PureComponent} from 'react';
-import Container              from 'react-bootstrap/Container';
-import Row                    from 'react-bootstrap/Row';
-import {connect}              from 'react-redux';
-import {bindActionCreators}   from 'redux';
+import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
+import {forbidExtraProps} from 'airbnb-prop-types';
+import PropTypes          from 'prop-types';
+import React, {useEffect} from 'react';
+import Container          from 'react-bootstrap/Container';
+import Row                from 'react-bootstrap/Row';
+import {connect}          from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 import {filterOnTags, subscribeToUser} from '../actions/actionCreators';
 
@@ -19,78 +19,85 @@ import PageHeader         from './PageHeader';
 import SearchForm         from './SearchForm';
 import TagForm            from './TagForm';
 
-class MyFavoritesGrid extends PureComponent
+const MyFavoritesGrid = ({
+	subscribeToUser,
+	filterOnTags,
+	user,
+	blueprintSummaries,
+	blueprintSummariesLoading,
+	location,
+	history,
+	staticContext,
+	match,
+}) =>
 {
-	static propTypes = forbidExtraProps({
-		subscribeToUser          : PropTypes.func.isRequired,
-		filterOnTags             : PropTypes.func.isRequired,
-		user                     : propTypes.userSchema,
-		blueprintSummaries       : propTypes.blueprintSummariesSchema,
-		blueprintSummariesLoading: PropTypes.bool,
-		location                 : propTypes.locationSchema,
-		history                  : propTypes.historySchema,
-		staticContext            : PropTypes.shape(forbidExtraProps({})),
-		match                    : PropTypes.shape(forbidExtraProps({
-			params : PropTypes.shape(forbidExtraProps({})).isRequired,
-			path   : PropTypes.string.isRequired,
-			url    : PropTypes.string.isRequired,
-			isExact: PropTypes.bool.isRequired,
-		})),
-	});
-
-	UNSAFE_componentWillMount()
+	useEffect(() =>
 	{
-		if (this.props.user)
+		if (user)
 		{
-			this.props.subscribeToUser(this.props.user.uid);
+			subscribeToUser(user.uid);
 		}
-	}
+	}, [user, subscribeToUser]);
 
-	render()
+	if (!user)
 	{
-		if (!this.props.user)
-		{
-			return (
-				<div className='p-5 rounded-lg jumbotron'>
-					<h1 className='display-4'>
-						My Favorites
-					</h1>
-					<p className='lead'>
-						Please log in with Google or GitHub in order to view your favorite blueprints.
-					</p>
-				</div>
-			);
-		}
-
-		if (this.props.blueprintSummariesLoading)
-		{
-			return (
-				<div className='p-5 rounded-lg jumbotron'>
-					<h1 className='display-4'>
-						<FontAwesomeIcon icon={faCog} spin />
-						{' Loading data'}
-					</h1>
-				</div>
-			);
-		}
-
 		return (
-			<Container fluid>
-				<PageHeader title='My Favorites' />
-				<Row>
-					<SearchForm />
-					<TagForm />
-				</Row>
-				<Row className='blueprint-grid-row justify-content-center'>
-					{
-						this.props.blueprintSummaries.map(blueprintSummary =>
-							<BlueprintThumbnail key={blueprintSummary.key} blueprintSummary={blueprintSummary} />)
-					}
-				</Row>
-			</Container>
+			<div className='p-5 rounded-lg jumbotron'>
+				<h1 className='display-4'>
+					My Favorites
+				</h1>
+				<p className='lead'>
+					Please log in with Google or GitHub in order to view your favorite blueprints.
+				</p>
+			</div>
 		);
 	}
-}
+
+	if (blueprintSummariesLoading)
+	{
+		return (
+			<div className='p-5 rounded-lg jumbotron'>
+				<h1 className='display-4'>
+					<FontAwesomeIcon icon={faCog} spin />
+					{' Loading data'}
+				</h1>
+			</div>
+		);
+	}
+
+	return (
+		<Container fluid>
+			<PageHeader title='My Favorites' />
+			<Row>
+				<SearchForm />
+				<TagForm />
+			</Row>
+			<Row className='blueprint-grid-row justify-content-center'>
+				{
+					blueprintSummaries.map(blueprintSummary =>
+						<BlueprintThumbnail key={blueprintSummary.key} blueprintSummary={blueprintSummary} />)
+				}
+			</Row>
+		</Container>
+	);
+};
+
+MyFavoritesGrid.propTypes = forbidExtraProps({
+	subscribeToUser          : PropTypes.func.isRequired,
+	filterOnTags             : PropTypes.func.isRequired,
+	user                     : propTypes.userSchema,
+	blueprintSummaries       : propTypes.blueprintSummariesSchema,
+	blueprintSummariesLoading: PropTypes.bool,
+	location                 : propTypes.locationSchema,
+	history                  : propTypes.historySchema,
+	staticContext            : PropTypes.shape(forbidExtraProps({})),
+	match                    : PropTypes.shape(forbidExtraProps({
+		params : PropTypes.shape(forbidExtraProps({})).isRequired,
+		path   : PropTypes.string.isRequired,
+		url    : PropTypes.string.isRequired,
+		isExact: PropTypes.bool.isRequired,
+	})),
+});
 
 const mapStateToProps = storeState => (
 	{
