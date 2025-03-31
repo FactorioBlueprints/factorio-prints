08378f56740da7097592e6210fb5d6f044607498
Factorio Blueprints <factorio.prints@gmail.com>
2025-03-31 13:47:32 -0400

    Convert TagForm from class to functional component

--- src/components/TagForm.js
+++ src/components/TagForm.js
@@ -1,6 +1,6 @@
-import {forbidExtraProps}     from 'airbnb-prop-types';
-import PropTypes              from 'prop-types';
-import React, {PureComponent} from 'react';
+import {forbidExtraProps}  from 'airbnb-prop-types';
+import PropTypes           from 'prop-types';
+import React, {useEffect}  from 'react';
 
 import Col       from 'react-bootstrap/Col';
 import {connect} from 'react-redux';
@@ -10,44 +10,47 @@ import {bindActionCreators} from 'redux';
 
 import {filterOnTags, subscribeToTag, subscribeToTags} from '../actions/actionCreators';
 
-class TagForm extends PureComponent
+const TagForm = ({
+	tags,
+	filteredTags,
+	subscribeToTag,
+	subscribeToTags,
+	filterOnTags,
+}) =>
 {
-	static propTypes = forbidExtraProps({
-		tags           : PropTypes.arrayOf(PropTypes.string).isRequired,
-		filteredTags   : PropTypes.arrayOf(PropTypes.string).isRequired,
-		subscribeToTag : PropTypes.func.isRequired,
-		subscribeToTags: PropTypes.func.isRequired,
-		filterOnTags   : PropTypes.func.isRequired,
-	});
-
-	UNSAFE_componentWillMount()
+	useEffect(() =>
 	{
-		this.props.subscribeToTags();
-	}
+		subscribeToTags();
+	}, [subscribeToTags]);
 
-	handleTagSelection = (filteredTags) =>
+	const handleTagSelection = (filteredTags) =>
 	{
 		const filteredTagStrings = filteredTags.map(each => each.value);
-		filteredTagStrings.forEach(this.props.subscribeToTag);
-		this.props.filterOnTags(filteredTagStrings);
+		filteredTagStrings.forEach(subscribeToTag);
+		filterOnTags(filteredTagStrings);
 	};
 
-	render()
-	{
-		return (
-			<Col md={6}>
-				<Select
-					value={this.props.filteredTags.map(value => ({value, label: value}))}
-					options={this.props.tags.map(value => ({value, label: value}))}
-					onChange={this.handleTagSelection}
-					isMulti
-					placeholder='search tags'
-					className='tag-form'
-				/>
-			</Col>
-		);
-	}
-}
+	return (
+		<Col md={6}>
+			<Select
+				value={filteredTags.map(value => ({value, label: value}))}
+				options={tags.map(value => ({value, label: value}))}
+				onChange={handleTagSelection}
+				isMulti
+				placeholder='search tags'
+				className='tag-form'
+			/>
+		</Col>
+	);
+};
+
+TagForm.propTypes = forbidExtraProps({
+	tags           : PropTypes.arrayOf(PropTypes.string).isRequired,
+	filteredTags   : PropTypes.arrayOf(PropTypes.string).isRequired,
+	subscribeToTag : PropTypes.func.isRequired,
+	subscribeToTags: PropTypes.func.isRequired,
+	filterOnTags   : PropTypes.func.isRequired,
+});
 
 const mapStateToProps = (state) =>
 {
