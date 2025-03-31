c6df45aa2ead98be13517c42adf5b84eafae95c0
Factorio Blueprints <factorio.prints@gmail.com>
2025-03-31 13:41:18 -0400

    Refactor BlueprintGrid component to functional component with hooks

--- src/components/BlueprintGrid.js
+++ src/components/BlueprintGrid.js
@@ -1,15 +1,15 @@
 import {faAngleDoubleLeft, faAngleLeft, faAngleRight, faCog} from '@fortawesome/free-solid-svg-icons';
 
-import {FontAwesomeIcon}      from '@fortawesome/react-fontawesome';
-import {forbidExtraProps}     from 'airbnb-prop-types';
-import PropTypes              from 'prop-types';
-import React, {PureComponent} from 'react';
-import Button                 from 'react-bootstrap/Button';
-import Col                    from 'react-bootstrap/Col';
-import Container              from 'react-bootstrap/Container';
-import Row                    from 'react-bootstrap/Row';
-import {connect}              from 'react-redux';
-import {bindActionCreators}   from 'redux';
+import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
+import {forbidExtraProps} from 'airbnb-prop-types';
+import PropTypes          from 'prop-types';
+import React, {useEffect} from 'react';
+import Button             from 'react-bootstrap/Button';
+import Col                from 'react-bootstrap/Col';
+import Container          from 'react-bootstrap/Container';
+import Row                from 'react-bootstrap/Row';
+import {connect}          from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 import {
 	filterOnTags,
@@ -29,120 +29,131 @@ import PageHeader         from './PageHeader';
 import SearchForm         from './SearchForm';
 import TagForm            from './TagForm';
 
-class BlueprintGrid extends PureComponent
+const BlueprintGrid = ({
+	initialTag,
+	subscribeToBlueprintSummaries,
+	goToPreviousSummaries,
+	goToNextSummaries,
+	goToFirstSummaries,
+	subscribeToUser,
+	filterOnTags,
+	subscribeToTag,
+	user,
+	blueprintSummaries,
+	blueprintSummariesLoading,
+	currentPage,
+	isLastPage,
+}) =>
 {
-	static propTypes = forbidExtraProps({
-		initialTag                   : PropTypes.string,
-		subscribeToBlueprintSummaries: PropTypes.func.isRequired,
-		goToPreviousSummaries        : PropTypes.func.isRequired,
-		goToNextSummaries            : PropTypes.func.isRequired,
-		goToFirstSummaries           : PropTypes.func.isRequired,
-		subscribeToUser              : PropTypes.func.isRequired,
-		filterOnTags                 : PropTypes.func.isRequired,
-		subscribeToTag               : PropTypes.func.isRequired,
-		user                         : propTypes.userSchema,
-		blueprintSummaries           : propTypes.blueprintSummariesSchema,
-		blueprintSummariesLoading    : PropTypes.bool,
-		currentPage                  : PropTypes.number.isRequired,
-		isLastPage                   : PropTypes.bool.isRequired,
-		location                     : propTypes.locationSchema,
-		history                      : propTypes.historySchema,
-		staticContext                : PropTypes.shape(forbidExtraProps({})),
-		match                        : PropTypes.shape(forbidExtraProps({
-			params : PropTypes.shape(forbidExtraProps({})).isRequired,
-			path   : PropTypes.string.isRequired,
-			url    : PropTypes.string.isRequired,
-			isExact: PropTypes.bool.isRequired,
-		})),
-	});
-
-	UNSAFE_componentWillMount()
+	useEffect(() =>
 	{
-		this.props.subscribeToBlueprintSummaries();
-		if (this.props.initialTag)
+		subscribeToBlueprintSummaries();
+		if (initialTag)
 		{
-			const formattedTagPath = this.props.initialTag.endsWith('/')
-				? this.props.initialTag
-				: this.props.initialTag + '/';
+			const formattedTagPath = initialTag.endsWith('/')
+				? initialTag
+				: initialTag + '/';
 
 			const tagPath = [formattedTagPath];
-			this.props.filterOnTags(tagPath);
-			this.props.subscribeToTag(formattedTagPath);
+			filterOnTags(tagPath);
+			subscribeToTag(formattedTagPath);
 		}
-		if (this.props.user)
+		if (user)
 		{
-			this.props.subscribeToUser(this.props.user.uid);
+			subscribeToUser(user.uid);
 		}
-	}
+	}, [initialTag, subscribeToBlueprintSummaries, filterOnTags, subscribeToTag, user, subscribeToUser]);
 
-	handlePreviousPage = () =>
+	const handlePreviousPage = () =>
 	{
 		window.scrollTo(0, 0);
-		this.props.goToPreviousSummaries();
+		goToPreviousSummaries();
 	};
 
-	handleNextPage = () =>
+	const handleNextPage = () =>
 	{
 		window.scrollTo(0, 0);
-		this.props.goToNextSummaries();
+		goToNextSummaries();
 	};
 
-	handleFirstPage = () =>
+	const handleFirstPage = () =>
 	{
 		window.scrollTo(0, 0);
-		this.props.goToFirstSummaries();
+		goToFirstSummaries();
 	};
 
-	render()
+	if (blueprintSummariesLoading)
 	{
-		if (this.props.blueprintSummariesLoading)
-		{
-			return (
-				<div className='p-5 rounded-lg jumbotron'>
-					<h1 className='display-4'>
-						<FontAwesomeIcon icon={faCog} spin />
-						{' Loading data'}
-					</h1>
-				</div>
-			);
-		}
-
 		return (
-			<Container fluid>
-				<PageHeader title='Most Recent' />
-				<Row>
-					<SearchForm />
-					<TagForm />
-				</Row>
-				<Row className='blueprint-grid-row justify-content-center'>
-					{
-						this.props.blueprintSummaries.map(blueprintSummary =>
-							<BlueprintThumbnail key={blueprintSummary.key} blueprintSummary={blueprintSummary} />)
-					}
-				</Row>
-				<Row>
-					<Col md={{span: 6, offset: 3}}>
-						<Button type='button' onClick={this.handleFirstPage} disabled={this.props.currentPage === 1} >
-							<FontAwesomeIcon icon={faAngleDoubleLeft} size='lg' fixedWidth />
-							{'First Page'}
-						</Button>
-						<Button type='button' onClick={this.handlePreviousPage} disabled={this.props.currentPage === 1}>
-							<FontAwesomeIcon icon={faAngleLeft} size='lg' fixedWidth />
-							{'Previous Page'}
-						</Button>
-						<Button variant='link' type='button' disabled>
-							{`Page: ${this.props.currentPage}`}
-						</Button>
-						<Button type='button' onClick={this.handleNextPage} disabled={this.props.isLastPage}>
-							{'Next Page'}
-							<FontAwesomeIcon icon={faAngleRight} size='lg' fixedWidth />
-						</Button>
-					</Col>
-				</Row>
-			</Container>
+			<div className='p-5 rounded-lg jumbotron'>
+				<h1 className='display-4'>
+					<FontAwesomeIcon icon={faCog} spin />
+					{' Loading data'}
+				</h1>
+			</div>
 		);
 	}
-}
+
+	return (
+		<Container fluid>
+			<PageHeader title='Most Recent' />
+			<Row>
+				<SearchForm />
+				<TagForm />
+			</Row>
+			<Row className='blueprint-grid-row justify-content-center'>
+				{
+					blueprintSummaries.map(blueprintSummary =>
+						<BlueprintThumbnail key={blueprintSummary.key} blueprintSummary={blueprintSummary} />)
+				}
+			</Row>
+			<Row>
+				<Col md={{span: 6, offset: 3}}>
+					<Button type='button' onClick={handleFirstPage} disabled={currentPage === 1} >
+						<FontAwesomeIcon icon={faAngleDoubleLeft} size='lg' fixedWidth />
+						{'First Page'}
+					</Button>
+					<Button type='button' onClick={handlePreviousPage} disabled={currentPage === 1}>
+						<FontAwesomeIcon icon={faAngleLeft} size='lg' fixedWidth />
+						{'Previous Page'}
+					</Button>
+					<Button variant='link' type='button' disabled>
+						{`Page: ${currentPage}`}
+					</Button>
+					<Button type='button' onClick={handleNextPage} disabled={isLastPage}>
+						{'Next Page'}
+						<FontAwesomeIcon icon={faAngleRight} size='lg' fixedWidth />
+					</Button>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+BlueprintGrid.propTypes = forbidExtraProps({
+	initialTag                   : PropTypes.string,
+	subscribeToBlueprintSummaries: PropTypes.func.isRequired,
+	goToPreviousSummaries        : PropTypes.func.isRequired,
+	goToNextSummaries            : PropTypes.func.isRequired,
+	goToFirstSummaries           : PropTypes.func.isRequired,
+	subscribeToUser              : PropTypes.func.isRequired,
+	filterOnTags                 : PropTypes.func.isRequired,
+	subscribeToTag               : PropTypes.func.isRequired,
+	user                         : propTypes.userSchema,
+	blueprintSummaries           : propTypes.blueprintSummariesSchema,
+	blueprintSummariesLoading    : PropTypes.bool,
+	currentPage                  : PropTypes.number.isRequired,
+	isLastPage                   : PropTypes.bool.isRequired,
+	location                     : propTypes.locationSchema,
+	history                      : propTypes.historySchema,
+	staticContext                : PropTypes.shape(forbidExtraProps({})),
+	match                        : PropTypes.shape(forbidExtraProps({
+		params : PropTypes.shape(forbidExtraProps({})).isRequired,
+		path   : PropTypes.string.isRequired,
+		url    : PropTypes.string.isRequired,
+		isExact: PropTypes.bool.isRequired,
+	})),
+});
 
 const mapStateToProps = storeState => (
 	{
