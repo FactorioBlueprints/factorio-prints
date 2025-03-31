ffe682c223961801204abfc443e7ea36353a62c5
Factorio Blueprints <factorio.prints@gmail.com>
2025-03-31 13:17:34 -0400

    Switch from yarn to npm with legacy-peer-deps flag

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 
 git pushf open-source HEAD:factorio-prints.com \
 	&& git pushf open-source HEAD:factorio-prints.com \
-	&& npm install --no-optional --ignore-engines --ignore-platform --ignore-scripts \
+	&& npm install --no-optional --ignore-engines --ignore-platform --ignore-scripts --legacy-peer-deps \
 	&& npm run build \
 	&& firebase deploy \
 	&& git tag prod HEAD -f \
