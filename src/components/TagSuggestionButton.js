46ad2992ad0ba4e4a2ad54810554bf58cc6e1032
Factorio Blueprints <factorio.prints@gmail.com>
2025-03-31 13:39:12 -0400

    Refactor TagSuggestionButton component to functional component with hooks

--- src/components/TagSuggestionButton.js
+++ src/components/TagSuggestionButton.js
@@ -1,34 +1,33 @@
-import {faTag}                from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon}      from '@fortawesome/react-fontawesome';
-import {forbidExtraProps}     from 'airbnb-prop-types';
-import PropTypes              from 'prop-types';
-import React, {PureComponent} from 'react';
-import Button                 from 'react-bootstrap/Button';
+import {faTag}            from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
+import {forbidExtraProps} from 'airbnb-prop-types';
+import PropTypes          from 'prop-types';
+import React              from 'react';
+import Button             from 'react-bootstrap/Button';
 
-export default class TagSuggestionButton extends PureComponent
+const TagSuggestionButton = ({tagSuggestion, addTag}) =>
 {
-	static propTypes = forbidExtraProps({
-		tagSuggestion: PropTypes.string.isRequired,
-		addTag       : PropTypes.func.isRequired,
-	});
-
-	handleClick = () =>
+	const handleClick = () =>
 	{
-		this.props.addTag(this.props.tagSuggestion);
+		addTag(tagSuggestion);
 	};
 
-	render()
-	{
-		return (
-			<Button
-				variant='warning'
-				type='button'
-				onClick={this.handleClick}
-			>
-				<FontAwesomeIcon icon={faTag} />
-				{' '}
-				{this.props.tagSuggestion}
-			</Button>
-		);
-	}
-}
+	return (
+		<Button
+			variant='warning'
+			type='button'
+			onClick={handleClick}
+		>
+			<FontAwesomeIcon icon={faTag} />
+			{' '}
+			{tagSuggestion}
+		</Button>
+	);
+};
+
+TagSuggestionButton.propTypes = forbidExtraProps({
+	tagSuggestion: PropTypes.string.isRequired,
+	addTag       : PropTypes.func.isRequired,
+});
+
+export default TagSuggestionButton;
