ae550e345985e14720d2acef2ea943ee979373f0
Factorio Blueprints <factorio.prints@gmail.com>
2025-04-30 18:37:55 -0400

    Add date-based production tag to deployment script.

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@
 
 set -exuo pipefail
 
+DATE_TAG="prod.$(date +%Y-%m-%d)"
+
 git pushf open-source HEAD:factorio-prints.com \
 	&& git pushf open-source HEAD:factorio-prints.com \
 	&& npm install --no-optional --ignore-engines --ignore-platform --ignore-scripts --legacy-peer-deps \
 	&& npm run build \
 	&& firebase deploy \
 	&& git tag prod HEAD -f \
-	&& git pushf open-source prod -f
+	&& git tag "$DATE_TAG" HEAD \
+	&& git pushf open-source prod -f \
+	&& git pushf open-source "$DATE_TAG"
