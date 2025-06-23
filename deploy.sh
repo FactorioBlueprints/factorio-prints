60d784fe71d70cfafb20209c7daa3e11e64e95d8
Factorio Blueprints <factorio.prints@gmail.com>
2025-06-23 15:34:17 -0400

    Fix deploy.sh to use op run for resolving 1Password Sentry token.

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ DATE_TAG="prod.$(date +%Y-%m-%d)"
 git pushf open-source HEAD:factorio-prints.com \
 	&& git pushf open-source HEAD:factorio-prints.com \
 	&& npm install --ignore-scripts --legacy-peer-deps \
-	&& npm run build \
+	&& op run -- npm run build \
 	&& firebase deploy \
 	&& git tag prod HEAD -f \
 	&& git tag "$DATE_TAG" HEAD -f \
