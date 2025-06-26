ef5df354bb0a79ba9634104a02c89a988e4715e4
Factorio Blueprints <factorio.prints@gmail.com>
2025-06-26 15:03:57 -0400

    Convert ErrorDisplay.jsx to TypeScript.

---
+++ src/components/grid/ErrorDisplay.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+
+interface ErrorDisplayProps {
+	error: unknown;
+	message?: string;
+}
+
+const ErrorDisplay = ({ error, message = 'There was a problem loading the data.' }: ErrorDisplayProps) =>
+{
+	if (!error) return null;
+
+	return (
+		<Row className='justify-content-center my-5'>
+			<div className='col-12 text-center'>
+				<h3>Error</h3>
+				<p>{message}</p>
+			</div>
+		</Row>
+	);
+};
+
+export default ErrorDisplay;
