518b8d45ae32384308dbe2136e6a997154843b0e
Factorio Blueprints <factorio.prints@gmail.com>
2025-07-22 23:27:38 -0400

    Fix routing structure for user favorites pages.

---
+++ src/routes/favorites.index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createFileRoute} from '@tanstack/react-router';
+import FavoritesGrid from '../components/MyFavoritesGrid';
+import ErrorBoundary from '../components/ErrorBoundary';
+
+export const Route = createFileRoute('/favorites/')({
+	component: FavoritesComponent,
+});
+
+function FavoritesComponent() {
+	return (
+		<ErrorBoundary>
+			<FavoritesGrid />
+		</ErrorBoundary>
+	);
+}
