558849e0ff43b3b4e45ac4f12c7c59e06caee02f
Factorio Blueprints <factorio.prints@gmail.com>
2025-08-04 13:42:08 -0400

    Add blueprint titles tree to Create preview and refactor layout.

--- src/components/BlueprintPreview.tsx
+++ src/components/BlueprintPreview.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import {useBlueprintHistograms} from '../hooks/useBlueprintHistograms';
 import {BlueprintWrapper} from '../parsing/BlueprintWrapper';
 import type {RawBlueprintData} from '../schemas';
 import {BasicInfoPanel} from './blueprint/panels/info/BasicInfoPanel';
-import {ExtraInfoPanel} from './blueprint/panels/info/ExtraInfoPanel';
 import {ParametersPanel} from './blueprint/panels/parameters/ParametersPanel';
 import {RequirementsCard} from './single/RequirementsCard';
 import BlueprintTitles from './single/BlueprintTitles';
@@ -29,34 +26,29 @@ export const BlueprintPreview: React.FC<BlueprintPreviewProps> = ({
 	}
 
 	return (
-		<Row>
-			<Col md={4}>
+		<div>
+			<BasicInfoPanel blueprint={decodedBlueprint} />
+			<Card className="mt-3">
+				<Card.Header>Blueprint Titles</Card.Header>
+				<Card.Body>
+					<BlueprintTitles
+						blueprintKey={blueprintKey}
+						parsedData={decodedBlueprint}
+						isLoading={false}
+					/>
+				</Card.Body>
+			</Card>
+			<div className="mt-3">
 				<RequirementsCard
 					blueprintWrapper={blueprintWrapper}
 					entityHistogram={entityHistogram}
 					itemHistogram={itemHistogram}
 				/>
-			</Col>
-			<Col md={8}>
-				<Card className="mb-3">
-					<Card.Header>Blueprint Titles</Card.Header>
-					<Card.Body>
-						<BlueprintTitles
-							blueprintKey={blueprintKey}
-							parsedData={decodedBlueprint}
-							isLoading={false}
-						/>
-					</Card.Body>
-				</Card>
-				<BasicInfoPanel blueprint={decodedBlueprint} />
-				<div className="mt-3">
-					<ExtraInfoPanel blueprint={decodedBlueprint} />
-				</div>
-				<div className="mt-3">
-					<ParametersPanel blueprintString={decodedBlueprint} />
-				</div>
-			</Col>
-		</Row>
+			</div>
+			<div className="mt-3">
+				<ParametersPanel blueprintString={decodedBlueprint} />
+			</div>
+		</div>
 	);
 };
 
